0a946968d45de7ea7e8981422747daae924f4248
Shoto Nishida <j520329b@mails.cc.ehime-u.ac.jp>
2025-06-04 01:34:18 +0900

    Add build_files for prob1-1, .. prob1-3

---
+++ build/prob1/prob1-2/CMakeFiles/prob1-2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prob1-2.dir/main.cpp.obj"
+  "CMakeFiles/prob1-2.dir/main.cpp.obj.d"
+  "libprob1-2.dll.a"
+  "prob1-2.exe"
+  "prob1-2.exe.manifest"
+  "prob1-2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prob1-2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/prob1/prob1-3/CMakeFiles/prob1-3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prob1-3.dir/main.cpp.obj"
+  "CMakeFiles/prob1-3.dir/main.cpp.obj.d"
+  "libprob1-3.dll.a"
+  "prob1-3.exe"
+  "prob1-3.exe.manifest"
+  "prob1-3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prob1-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
