5cda7989fe2b26b4b28d70ecceb7573a0e41b561
Shoto Nishida <j520329b@mails.cc.ehime-u.ac.jp>
2025-06-04 01:27:53 +0900

    Add CMakeLists for prob1

---
+++ prob1/prob1-5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s, t;
+    cout << "1つ目の数:";
+    cin >> s;
+    cout << "2つ目の数:";
+    cin >> t;
+    cout << s << " + " << t << " = " << s+t << "\n";
+    cout << s << " - " << t << " = " << s-t << endl;
+    return 0;
+}
